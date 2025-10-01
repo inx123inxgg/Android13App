c5d1eed3c9535892e2f49758455c7f4bf42eab5c
inx123inxgg <inxgaming55223355@gmail.com>
2025-10-01 18:39:38 +0530

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem Gradle startup script for Windows
-@rem ##########################################################################
-
-set DIR=%~dp0
-set GRADLE_USER_HOME=%DIR%\.gradle
-
-java -Xmx64m -cp "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
